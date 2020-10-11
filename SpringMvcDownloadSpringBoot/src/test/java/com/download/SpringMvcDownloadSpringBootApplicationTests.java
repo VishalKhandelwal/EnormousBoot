b1258190fa14c0f2be04d74511a07c237e562019
Vishal Khandelwal <vishalshekhar9316@gmail.com>
2020-10-11 17:23:54 +0530

    File Download

---
+++ SpringMvcDownloadSpringBoot/src/test/java/com/download/SpringMvcDownloadSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.download;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcDownloadSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
